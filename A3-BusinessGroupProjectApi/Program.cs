3be12ef1528f56f1b38d2a38911134d188e957ea
mkbcodes <mkbcodes@gmail.com>
2023-03-10 18:14:46 -0500

    Added in Accept headers for json and xml requests

--- A3-BusinessGroupProjectApi/Program.cs
+++ A3-BusinessGroupProjectApi/Program.cs
@@ -1,17 +1,63 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddXmlDataContractSerializerFormatters()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Use((context, next) =>
+{
+    if (context.Request.Headers.ContainsKey("Accept"))
+    {
+        var mediaType = context.Request.Headers["Accept"].ToString().ToLowerInvariant();
+
+        if (mediaType == "application/xml")
+        {
+            context.Request.Headers["Accept"] = "application/xml";
+            context.Response.ContentType = "application/xml";
+        }
+        else if (mediaType == "application/json")
+        {
+            context.Request.Headers["Accept"] = "application/json";
+            context.Response.ContentType = "application/json";
+        }
+        else
+        {
+            context.Response.StatusCode = 406;
+            return Task.CompletedTask;
+        }
+    }
+    else
+    {
+        context.Request.Headers["Accept"] = "application/json";
+        context.Response.ContentType = "application/json";
+    }
+
+    return next();
+});
 
 app.Run();
